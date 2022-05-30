f66e71494627570022837efae633faaf1c93bdd0
温海波 <wenhb@techsure.com.cn>
2022-05-30 20:41:32 +0800

    windows uninstall 脚本问题

--- service-uninstall.bat
+++ service-uninstall.bat
@@ -1,3 +1,3 @@
 net stop Tagent-Server
 tssm remove Tagent-Server confirm
-"%CD%\Perl\bin\perl" -i -pe "s/tagent.id=.*/tagent.id/" "%CD%\conf\tagent.conf"
+"%CD%\Perl\bin\perl" -i.bak -pe "s/tagent.id=.*/tagent.id/" "%CD%\conf\tagent.conf"
