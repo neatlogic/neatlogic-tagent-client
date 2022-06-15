e77dd6f088bef4e58bf3c9c80b80de456c41d3c6
温海波 <wenhb@techsure.com.cn>
2022-06-15 12:52:19 +0800

    安装脚本

--- service-install.bat
+++ service-install.bat
@@ -7,10 +7,10 @@ IF NOT "%TENANT%" == "" (
 ) 
 
 IF NOT "%SRV_ADDR%" == "" (
-    set REG_URL="%SRV_ADDR%/autoexecrunner/public/api/rest/tagent/register?tenant=%TENANT%"
-    setlocal EnableDelayedExpansion
-    "%CD%\Perl\bin\perl" -i.bak -pe "$regUrl='%REG_URL%'; s/proxy\.registeraddress=.*/proxy.registeraddress=$regUrl/g" "%CD%\conf\tagent.conf"
-) 
+    set REG_URL=%SRV_ADDR%/autoexecrunner/public/api/rest/tagent/register?tenant=%TENANT%
+    "%CD%\Perl\bin\perl" -i.bak -pe "s/proxy\.registeraddress=.*/proxy.registeraddress=$ENV{REG_URL}/g" "%CD%\conf\tagent.conf"
+)
+
 echo on
 
 @REM 请使用管理员运行cmd，运行此脚本进行服务安装
